7a18c616f97ab2e0316ea5a3cdd8f896202f76c1
Inwerwm <saclainvervm@gmail.com>
2021-02-11 13:49:23 +0900

    位置四角形用構造体の中身をテクスチャ板用のと同じにした

--- PEPIwUVEditor/DirectX/Vertex/PositionVertex.cs
+++ PEPIwUVEditor/DirectX/Vertex/PositionVertex.cs
@@ -12,10 +12,14 @@ namespace IwUVEditor.DirectX.Vertex
     struct PositionVertex
     {
         public Vector3 Position;
+        public Color4 Color;
+        public Vector2 TEXCOORD;
 
         public static readonly InputElement[] VertexElements = new[]
         {
-            new InputElement("Position", 0, Format.R32G32B32_Float,    0,                          0, InputClassification.PerVertexData,   0),
+            new InputElement("SV_Position", 0, Format.R32G32B32_Float,    0,                          0, InputClassification.PerVertexData,   0),
+            new InputElement("Color",       0, Format.R32G32B32A32_Float, InputElement.AppendAligned, 0, InputClassification.PerVertexData,   0),
+            new InputElement("TEXCOORD",    0, Format.R32G32_Float,       InputElement.AppendAligned, 0, InputClassification.PerVertexData,   0),
         };
 
         public static int SizeInBytes

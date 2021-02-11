324886bd596d29c627476b0a07feaa8d4b65cfbc
Inwerwm <saclainvervm@gmail.com>
2021-02-11 19:21:40 +0900

    コマンド関連を専門クラスに切り分け

---
+++ PEPIwUVEditor/Command/CommandManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IwUVEditor.Command
+{
+    class CommandManager
+    {
+        private Stack<IEditorCommand> UndoStack { get; }
+        private Stack<IEditorCommand> RedoStack { get; }
+
+        public void Do(IEditorCommand cmd)
+        {
+            cmd.Do();
+            UndoStack.Push(cmd);
+            RedoStack.Clear();
+        }
+
+        /// <summary>
+        /// 元に戻す
+        /// </summary>
+        public void Undo()
+        {
+            if (!UndoStack.Any())
+                return;
+
+            var com = UndoStack.Pop();
+            com.Undo();
+            RedoStack.Push(com);
+        }
+
+        /// <summary>
+        /// やり直し
+        /// </summary>
+        public void Redo()
+        {
+            if (!RedoStack.Any())
+                return;
+
+            var com = RedoStack.Pop();
+            com.Do();
+            UndoStack.Push(com);
+        }
+    }
+}

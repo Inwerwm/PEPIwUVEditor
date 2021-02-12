e2bf01ed70663c27ca27aa1b4f003c1c2fc5d4ef
Inwerwm <saclainvervm@gmail.com>
2021-02-13 01:10:05 +0900

    矩形選択表示描画要素のクラスを追加

---
+++ PEPIwUVEditor/DirectX/DrawElement/SelectionRectangle.cs
@@ -0,0 +1,65 @@
+﻿using IwUVEditor.DirectX.Vertex;
+using SlimDX;
+using SlimDX.Direct3D11;
+using SlimDX.DXGI;
+using System;
+using System.Linq;
+using Buffer = SlimDX.Direct3D11.Buffer;
+using Device = SlimDX.Direct3D11.Device;
+
+namespace IwUVEditor.DirectX.DrawElement
+{
+    class SelectionRectangle : IDrawElement
+    {
+        private bool disposedValue;
+
+        Device Device { get; }
+        EffectPass UsingEffectPass { get; }
+        public RasterizerState DrawMode { get; set; }
+
+        InputLayout VertexLayout { get; set; }
+
+        Buffer VertexBuffer { get; set; }
+        Buffer IndexBuffer { get; set; }
+
+        public Color4 Color { get; set; }
+
+
+        public void Prepare()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: マネージド状態を破棄します (マネージド オブジェクト)
+                    VertexLayout?.Dispose();
+                    VertexBuffer?.Dispose();
+                    IndexBuffer?.Dispose();
+                }
+
+                // TODO: アンマネージド リソース (アンマネージド オブジェクト) を解放し、ファイナライザーをオーバーライドします
+                // TODO: 大きなフィールドを null に設定します
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: 'Dispose(bool disposing)' にアンマネージド リソースを解放するコードが含まれる場合にのみ、ファイナライザーをオーバーライドします
+        // ~SelectionRectangle()
+        // {
+        //     // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}

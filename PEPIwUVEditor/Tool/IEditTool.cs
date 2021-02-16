6dfec2b76e6315a78c4777f0359e9d2cecf68713
Inwerwm <saclainvervm@gmail.com>
2021-02-16 14:12:36 +0900

    ツール用インターフェイスと矩形選択ツールを作成

---
+++ PEPIwUVEditor/Tool/IEditTool.cs
@@ -0,0 +1,18 @@
+﻿using IwUVEditor.Command;
+using IwUVEditor.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IwUVEditor.Tool
+{
+    interface IEditTool
+    {
+        bool IsReady { get; }
+
+        void ReadMouse(DragManager mouse);
+        IEditorCommand CreateCommand();
+    }
+}
---
+++ PEPIwUVEditor/Tool/RectangleSelection.cs
@@ -0,0 +1,26 @@
+﻿using IwUVEditor.Command;
+using IwUVEditor.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IwUVEditor.Tool
+{
+    class RectangleSelection : IEditTool
+    {
+        public bool IsReady => throw new NotImplementedException();
+
+        public IEditorCommand CreateCommand()
+        {
+            if (!IsReady)
+                throw new InvalidOperationException();
+        }
+
+        public void ReadMouse(DragManager mouse)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

9cf1f5207b2e09c49189e1a7fd5a9f7229a0ccb9
Inwerwm <saclainvervm@gmail.com>
2021-02-19 10:49:13 +0900

    移動編集用クラスを追加

---
+++ PEPIwUVEditor/Command/CommandMoveVertices.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IwUVEditor.Command
+{
+    class CommandMoveVertices : IEditorCommand
+    {
+        public void Do()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Undo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PEPIwUVEditor/Tool/MoveVertices.cs
@@ -0,0 +1,62 @@
+﻿using IwUVEditor.Command;
+using IwUVEditor.Manager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IwUVEditor.Tool
+{
+    class MoveVertices : IEditTool
+    {
+        private bool disposedValue;
+
+        public bool IsReady { get; private set; }
+
+        public IEditorCommand CreateCommand(Material target)
+        {
+            return new CommandMoveVertices();
+        }
+
+        public void PrepareDrawing()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReadInput(DragManager mouse, Dictionary<Keys, bool> pressKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: マネージド状態を破棄します (マネージド オブジェクト)
+                }
+
+                // TODO: アンマネージド リソース (アンマネージド オブジェクト) を解放し、ファイナライザーをオーバーライドします
+                // TODO: 大きなフィールドを null に設定します
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: 'Dispose(bool disposing)' にアンマネージド リソースを解放するコードが含まれる場合にのみ、ファイナライザーをオーバーライドします
+        // ~MoveVertices()
+        // {
+        //     // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}

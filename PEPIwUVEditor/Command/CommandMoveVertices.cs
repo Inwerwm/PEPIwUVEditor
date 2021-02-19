732a3c499ee1ba103798c393a98f2c1daab65a17
Inwerwm <saclainvervm@gmail.com>
2021-02-19 12:18:13 +0900

    Do/Undoを実装

--- PEPIwUVEditor/Command/CommandMoveVertices.cs
+++ PEPIwUVEditor/Command/CommandMoveVertices.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PEPlugin.Pmx;
+using SlimDX;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,14 +10,23 @@ namespace IwUVEditor.Command
 {
     class CommandMoveVertices : IEditorCommand
     {
+        List<IPXVertex> TargetVertices { get; }
+        Matrix Offset { get; }
+
+        public CommandMoveVertices(List<IPXVertex> targetVertices, Matrix offset)
+        {
+            TargetVertices = targetVertices;
+            Offset = offset;
+        }
+
         public void Do()
         {
-            throw new NotImplementedException();
+            TargetVertices.AsParallel().ForAll(vtx => vtx.UV = Vector2.TransformCoordinate(vtx.UV, Offset));
         }
 
         public void Undo()
         {
-            throw new NotImplementedException();
+            TargetVertices.AsParallel().ForAll(vtx => vtx.UV = Vector2.TransformCoordinate(vtx.UV, Matrix.Invert(Offset)));
         }
     }
 }

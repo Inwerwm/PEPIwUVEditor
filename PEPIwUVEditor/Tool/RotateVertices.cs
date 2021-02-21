a963efdc17e4d093766efa3704a1811cdf62bcf8
Inwerwm <saclainvervm@gmail.com>
2021-02-21 12:10:42 +0900

    回転ツールを実装

---
+++ PEPIwUVEditor/Tool/RotateVertices.cs
@@ -0,0 +1,17 @@
+﻿using IwUVEditor.DirectX;
+using SlimDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IwUVEditor.Tool
+{
+    class RotateVertices : EditVertices, IEditTool
+    {
+        protected override Matrix Offset => Matrix.RotationZ(CurrentPos.Y - StartPos.Y);
+
+        public RotateVertices(UVViewDrawProcess process) : base(process) { }
+    }
+}

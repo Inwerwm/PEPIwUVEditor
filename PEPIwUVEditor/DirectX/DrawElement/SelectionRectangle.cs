a856c42636588d65db9fa54e56384fac02bed3c8
Inwerwm <saclainvervm@gmail.com>
2021-03-02 14:15:19 +0900

    描画要素の共通部分を基底クラスに移譲

--- PEPIwUVEditor/DirectX/DrawElement/SelectionRectangle.cs
+++ PEPIwUVEditor/DirectX/DrawElement/SelectionRectangle.cs
@@ -8,19 +8,9 @@ using Device = SlimDX.Direct3D11.Device;
 
 namespace IwUVEditor.DirectX.DrawElement
 {
-    class SelectionRectangle : IDrawElement
+    class SelectionRectangle : DrawElement<VertexStruct>, IDrawElement
     {
-        private bool disposedValue;
         private Color4 color;
-
-        Device Device { get; }
-        EffectPass UsingEffectPass { get; }
-        public RasterizerState DrawMode { get; set; }
-
-        InputLayout VertexLayout { get; set; }
-
-        Buffer VertexBuffer { get; set; }
-        Buffer IndexBuffer { get; set; }
 
         public Color4 Color
         {
@@ -35,80 +25,27 @@ namespace IwUVEditor.DirectX.DrawElement
         public Vector2 StartPos { get; set; }
         public Vector2 EndPos { get; set; }
 
-        public SelectionRectangle(Device device, Effect effect, RasterizerState drawMode, Color4 color)
+        public SelectionRectangle(Device device, Effect effect, RasterizerState drawMode, Color4 color):
+            base(device, effect.GetTechniqueByName("MainTechnique").GetPassByName("DrawVertexColorPass"), drawMode)
         {
-            Device = device;
-            UsingEffectPass = effect.GetTechniqueByName("MainTechnique").GetPassByName("DrawVertexColorPass");
-            DrawMode = drawMode;
-            Color = color;
+            this.color = color;
 
-            CreateVertexLayout();
-            CreateIndexBuffer();
+            Initialize();
         }
 
-        public void Prepare()
+        public override void Prepare()
         {
-            // 描画方式を設定
-            UsingEffectPass.Apply(Device.ImmediateContext);
-            Device.ImmediateContext.Rasterizer.State = DrawMode;
-            Device.ImmediateContext.InputAssembler.PrimitiveTopology = PrimitiveTopology.TriangleStrip;
-
-            // レイアウトを設定
-            Device.ImmediateContext.InputAssembler.InputLayout = VertexLayout;
-
-            // バッファを設定
             CreateVertexBuffer();
-            Device.ImmediateContext.InputAssembler.SetVertexBuffers(
-                0,
-                new VertexBufferBinding(VertexBuffer, VertexStruct.SizeInBytes, 0)
-            );
-            Device.ImmediateContext.InputAssembler.SetIndexBuffer(IndexBuffer, Format.R32_UInt, 0);
+            base.Prepare();
+        }
 
+        protected override void DrawToDevice()
+        {
             Device.ImmediateContext.DrawIndexed(3, 0, 0);
             Device.ImmediateContext.DrawIndexed(3, 3, 0);
         }
 
-        void CreateVertexLayout()
-        {
-            VertexLayout?.Dispose();
-            VertexLayout = new InputLayout(
-                Device,
-                UsingEffectPass.Description.Signature,
-                VertexStruct.VertexElements
-            );
-        }
-
-        void CreateVertexBuffer()
-        {
-            VertexBuffer?.Dispose();
-            using (var vertexStream = new DataStream(CreateVertices(), true, true))
-                VertexBuffer = new Buffer(
-                    Device,
-                    vertexStream,
-                    new BufferDescription
-                    {
-                        SizeInBytes = (int)vertexStream.Length,
-                        BindFlags = BindFlags.VertexBuffer,
-                    }
-                );
-        }
-
-        void CreateIndexBuffer()
-        {
-            IndexBuffer?.Dispose();
-            using (DataStream indexStream = new DataStream(CreateIndices(), true, true))
-                IndexBuffer = new Buffer(
-                    Device,
-                    indexStream,
-                    new BufferDescription
-                    {
-                        SizeInBytes = (int)indexStream.Length,
-                        BindFlags = BindFlags.IndexBuffer,
-                    }
-                );
-        }
-
-        private VertexStruct[] CreateVertices()
+        protected override VertexStruct[] CreateVertices()
         {
             Vector2 min = new Vector2(Math.Min(StartPos.X, EndPos.X), Math.Min(StartPos.Y, EndPos.Y));
             Vector2 max = new Vector2(Math.Max(StartPos.X, EndPos.X), Math.Max(StartPos.Y, EndPos.Y));
@@ -142,42 +79,9 @@ namespace IwUVEditor.DirectX.DrawElement
             };
         }
 
-        private uint[] CreateIndices() => new uint[] {
+        protected override uint[] CreateIndices() => new uint[] {
             0, 1, 2,
             3, 2, 1
         };
-
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    // TODO: マネージド状態を破棄します (マネージド オブジェクト)
-                    VertexLayout?.Dispose();
-                    VertexBuffer?.Dispose();
-                    IndexBuffer?.Dispose();
-                }
-
-                // TODO: アンマネージド リソース (アンマネージド オブジェクト) を解放し、ファイナライザーをオーバーライドします
-                // TODO: 大きなフィールドを null に設定します
-                disposedValue = true;
-            }
-        }
-
-        // // TODO: 'Dispose(bool disposing)' にアンマネージド リソースを解放するコードが含まれる場合にのみ、ファイナライザーをオーバーライドします
-        // ~SelectionRectangle()
-        // {
-        //     // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
-        //     Dispose(disposing: false);
-        // }
-
-        public void Dispose()
-        {
-            // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
-        }
     }
 }
--- PEPIwUVEditor/DirectX/DrawElement/UVMesh.cs
+++ PEPIwUVEditor/DirectX/DrawElement/UVMesh.cs
@@ -10,19 +10,9 @@ using Device = SlimDX.Direct3D11.Device;
 
 namespace IwUVEditor.DirectX.DrawElement
 {
-    class UVMesh : IDrawElement
+    class UVMesh : DrawElement<VertexStruct>, IDrawElement
     {
-        private bool disposedValue;
         private Color4 lineColor;
-
-        Device Device { get; }
-        EffectPass UsingEffectPass { get; }
-        public RasterizerState DrawMode { get; set; }
-
-        InputLayout VertexLayout { get; set; }
-
-        Buffer VertexBuffer { get; set; }
-        Buffer IndexBuffer { get; set; }
 
         Material SourceMaterial { get; }
 
@@ -36,106 +26,51 @@ namespace IwUVEditor.DirectX.DrawElement
             }
         }
 
-        public UVMesh(Device device, Effect effect, RasterizerState drawMode, Material material, Color4 lineColor)
+        public UVMesh(Device device, Effect effect, RasterizerState drawMode, Material material, Color4 lineColor):
+            base(device, effect.GetTechniqueByName("MainTechnique").GetPassByName("DrawVertexColorPass"), drawMode)
         {
-            Device = device;
-            UsingEffectPass = effect.GetTechniqueByName("MainTechnique").GetPassByName("DrawVertexColorPass");
-            DrawMode = drawMode;
             SourceMaterial = material;
             this.lineColor = lineColor;
 
             if (SourceMaterial is null)
                 return;
 
-            CreateVertexLayout();
-
-            CreateVertexBuffer();
-            CreateIndexBuffer();
+            Initialize();
         }
 
-        public void Prepare()
+        public override void Prepare()
         {
             if (SourceMaterial is null)
                 return;
 
-            // 描画方式を設定
-            UsingEffectPass.Apply(Device.ImmediateContext);
-            Device.ImmediateContext.Rasterizer.State = DrawMode;
-            Device.ImmediateContext.InputAssembler.PrimitiveTopology = PrimitiveTopology.TriangleStrip;
+            base.Prepare();
+        }
 
-            // レイアウトを設定
-            Device.ImmediateContext.InputAssembler.InputLayout = VertexLayout;
-
-            // バッファを設定
-            Device.ImmediateContext.InputAssembler.SetVertexBuffers(
-                0,
-                new VertexBufferBinding(VertexBuffer, VertexStruct.SizeInBytes, 0)
-            );
-            Device.ImmediateContext.InputAssembler.SetIndexBuffer(IndexBuffer, Format.R32_UInt, 0);
-
+        protected override void DrawToDevice()
+        {
             for (int i = 0; i < SourceMaterial.Faces.Count; i++)
             {
                 Device.ImmediateContext.DrawIndexed(3, i * 3, 0);
             }
         }
 
-        public void UpdateVertices()
+        public override void UpdateVertices()
         {
             if (SourceMaterial is null)
                 return;
             CreateVertexBuffer();
         }
 
-        void CreateVertexLayout()
-        {
-            VertexLayout?.Dispose();
-            VertexLayout = new InputLayout(
-                Device,
-                UsingEffectPass.Description.Signature,
-                VertexStruct.VertexElements
-            );
-        }
-
-        void CreateVertexBuffer()
-        {
-            VertexBuffer?.Dispose();
-
-            using (var vertexStream = new DataStream(LoadUVVertices(SourceMaterial), true, true))
-                VertexBuffer = new Buffer(
-                    Device,
-                    vertexStream,
-                    new BufferDescription
-                    {
-                        SizeInBytes = (int)vertexStream.Length,
-                        BindFlags = BindFlags.VertexBuffer,
-                    }
-                );
-        }
-
-        void CreateIndexBuffer()
-        {
-            IndexBuffer?.Dispose();
-
-            using (DataStream indexStream = new DataStream(SourceMaterial.FaceSequence, true, true))
-                IndexBuffer = new Buffer(
-                    Device,
-                    indexStream,
-                    new BufferDescription
-                    {
-                        SizeInBytes = (int)indexStream.Length,
-                        BindFlags = BindFlags.IndexBuffer,
-                    }
-                );
-        }
-
-        private VertexStruct[] LoadUVVertices(Material material) =>
-            material.Vertices.Select(vtx => new VertexStruct()
+        protected override VertexStruct[] CreateVertices() =>
+            SourceMaterial.Vertices.Select(vtx => new VertexStruct()
             {
-                Position = Vector3.TransformCoordinate(new Vector3(vtx.UV.X, vtx.UV.Y, 0), material.TemporaryTransformMatrices[vtx]),
+                Position = Vector3.TransformCoordinate(new Vector3(vtx.UV.X, vtx.UV.Y, 0), SourceMaterial.TemporaryTransformMatrices[vtx]),
                 Color = LineColor,
                 TEXCOORD = vtx.UV
             }
             ).ToArray();
+
+        protected override uint[] CreateIndices() => SourceMaterial.FaceSequence;
 
         protected virtual void Dispose(bool disposing)
         {

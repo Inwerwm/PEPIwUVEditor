f0457fa7b357eff125793d06a5c251b4baa9504d
Inwerwm <saclainvervm@gmail.com>
2021-03-02 15:12:50 +0900

    基底クラスに移譲しながら消し忘れていたDisposeを削除

--- PEPIwUVEditor/DirectX/DrawElement/UVMesh.cs
+++ PEPIwUVEditor/DirectX/DrawElement/UVMesh.cs
@@ -71,37 +71,5 @@ namespace IwUVEditor.DirectX.DrawElement
             ).ToArray();
 
         protected override uint[] CreateIndices() => SourceMaterial.FaceSequence;
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    // TODO: マネージド状態を破棄します (マネージド オブジェクト)
-                    VertexLayout?.Dispose();
-                    VertexBuffer?.Dispose();
-                    IndexBuffer?.Dispose();
-                }
-
-                // TODO: アンマネージド リソース (アンマネージド オブジェクト) を解放し、ファイナライザーをオーバーライドします
-                // TODO: 大きなフィールドを null に設定します
-                disposedValue = true;
-            }
-        }
-
-        // // TODO: 'Dispose(bool disposing)' にアンマネージド リソースを解放するコードが含まれる場合にのみ、ファイナライザーをオーバーライドします
-        // ~UVMesh()
-        // {
-        //     // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
-        //     Dispose(disposing: false);
-        // }
-
-        public void Dispose()
-        {
-            // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
-        }
     }
 }

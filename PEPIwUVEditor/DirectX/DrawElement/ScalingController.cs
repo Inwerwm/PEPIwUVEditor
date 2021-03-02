eeaf21f11106579b8c6b9655c2d08846b58ea73e
Inwerwm <saclainvervm@gmail.com>
2021-03-02 15:40:35 +0900

    拡大コントローラの描画クラスを追加

---
+++ PEPIwUVEditor/DirectX/DrawElement/ScalingController.cs
@@ -0,0 +1,32 @@
+﻿using IwUVEditor.DirectX.Vertex;
+using SlimDX.Direct3D11;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IwUVEditor.DirectX.DrawElement
+{
+    class ScalingController : DrawElement<VertexStruct>, IDrawElement
+    {
+        public ScalingController(Device device, EffectPass usingEffectPass, RasterizerState drawMode) : base(device, usingEffectPass, drawMode)
+        {
+        }
+
+        protected override uint[] CreateIndices()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override VertexStruct[] CreateVertices()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void DrawToDevice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

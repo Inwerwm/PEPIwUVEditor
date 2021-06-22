98a541d67255b3a2e28600f42090f41ea2679457
Inwerwm <saclainvervm@gmail.com>
2021-06-23 00:12:46 +0900

    回転中心コントローラーの管理を別クラスに移譲

--- PEPIwUVEditor/Tool/RotateVertices.cs
+++ PEPIwUVEditor/Tool/RotateVertices.cs
@@ -1,5 +1,6 @@
 ﻿using IwUVEditor.DirectX;
 using IwUVEditor.DirectX.DrawElement;
+using IwUVEditor.EditController;
 using IwUVEditor.StateContainer;
 using SlimDX;
 using System;
@@ -12,113 +13,35 @@ namespace IwUVEditor.Tool
 {
     class RotateVertices : EditVertices, IEditTool
     {
-        private Vector3 rotationCenter;
-
-        private RotationCenterSign CenterSign { get; set; }
-        private Vector3 RotationCenter
-        {
-            get => rotationCenter; 
-            set
-            {
-                rotationCenter = value;
-                CenterSign.Center = rotationCenter;
-            }
-        }
-        public float Radius { get; set; } = 0.025f;
-
-        SlimDX.Direct3D11.Device Device { get; }
-
-        Vector2 ScreenSize { get; set; }
+        RotationController Controller { get; }
 
         /// <summary>
         /// 回転量/移動量
         /// </summary>
         private static double Step => 0.001;
         protected override Matrix Offset =>
-            Matrix.Translation(RotationCenter * -1) * Matrix.RotationZ((float)(Step * Input.MouseOffset.Y * 2 * Math.PI)) * Matrix.Translation(RotationCenter);
-
-        private Mode CurrentMode { get; set; }
+            Matrix.Translation(Controller.Center * -1) * Matrix.RotationZ((float)(Step * Input.MouseOffset.Y * 2 * Math.PI)) * Matrix.Translation(Controller.Center);
 
         public RotateVertices(SlimDX.Direct3D11.Device device, UVViewDrawProcess process) : base(process)
         {
-            Device = device;
-
-            ScreenSize = Process.ScreenSize;
-            CenterSign = new RotationCenterSign(
-                Device,
-                Process.Effect,
-                Process.Rasterize.Solid,
-                CenterPos,
-                Radius,
-                new Color4(0, 0, 1),
-                ScreenSize
-            );
+            Controller = new RotationController(process, device);
         }
 
         public override void Initialize()
         {
             base.Initialize();
-
-            RotationCenter = CenterPos;
+            Controller.Center = CenterPos;
         }
 
         public override void PrepareDrawing()
         {
             base.PrepareDrawing();
-            if (ScreenSize != Process.ScreenSize)
-            {
-                CenterSign.ScreenSize = Process.ScreenSize;
-                ScreenSize = Process.ScreenSize;
-            }
-            CenterSign.Prepare();
+            Controller.PrepareDrawing();
         }
 
         public override void ReadInput(InputStates input)
         {
-            var modeTmp = CurrentMode;
-
-            if (!(input.MouseLeft.IsDragging || input.MouseLeft.IsEndingJust))
-                CurrentMode = CheckMode(input.MousePos);
-
-            switch (CurrentMode)
-            {
-                case Mode.Rotation:
-                    base.ReadInput(input);
-                    break;
-                case Mode.MoveCenter:
-                    MoveCenter(input);
-                    break;
-                default:
-                    break;
-            }
-
-            // ドラッグ終了時点の場合はモードの再読込を行わないと
-            // 次のマウス入力までモードが持続してしまう
-            if (input.MouseLeft.IsEndingJust)
-                CurrentMode = CheckMode(input.MousePos);
-
-            if (modeTmp != CurrentMode)
-                CenterSign.Color = CurrentMode == Mode.MoveCenter ? new Color4(1, 0, 1) : new Color4(0, 0, 1);
-
-            Mode CheckMode(Vector2 mousePos)
-            {
-                var vecOfRotationCenterToMouseCursor = mousePos - Process.WorldPosToScreenPos(new Vector2(RotationCenter.X, RotationCenter.Y));
-                var normalizedRelationalCursorPosition = vecOfRotationCenterToMouseCursor.ElementDivision(Process.ScreenSize);
-
-                // 正規化された相対的カーソル位置は [0,1] の値域で計算されている
-                // なので [-1,1] の値域での長さは 2 倍された値になる
-                float distanceFromRotationCenter = normalizedRelationalCursorPosition.Length() * 2;
-
-                return distanceFromRotationCenter < Radius ? Mode.MoveCenter : Mode.Rotation;
-            }
-        }
-
-        private void MoveCenter(InputStates input)
-        {
-            if (input.MouseLeft.IsDragging)
-            {
-                RotationCenter += new Vector3(input.MouseLeft.Offset, 0);
-            }
+            Controller.ReadInput(input, base.ReadInput);
         }
 
         protected override void Dispose(bool disposing)
@@ -127,17 +50,11 @@ namespace IwUVEditor.Tool
             {
                 if (disposing)
                 {
-                    CenterSign?.Dispose();
+                    Controller?.Dispose();
                 }
             }
 
             base.Dispose(disposing);
-        }
-
-        enum Mode
-        {
-            Rotation,
-            MoveCenter,
         }
     }
 }

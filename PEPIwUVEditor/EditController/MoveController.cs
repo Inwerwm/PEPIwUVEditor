6bfd1624363a04e1f8097028f275eb1729824d96
Inwerwm <saclainvervm@gmail.com>
2021-06-23 11:04:18 +0900

    MoveControllerのファイル概形を追加

---
+++ PEPIwUVEditor/EditController/MoveController.cs
@@ -0,0 +1,35 @@
+﻿using IwUVEditor.DirectX;
+using IwUVEditor.StateContainer;
+using SlimDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IwUVEditor.EditController
+{
+    class MoveController : EditController
+    {
+        public override Vector3 Center { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public MoveController(UVViewDrawProcess process) : base(process)
+        {
+        }
+
+        public override void PrepareDrawing()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void ApplyModeChange(SelectionMode mode)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override SelectionMode CalcMode(InputStates input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

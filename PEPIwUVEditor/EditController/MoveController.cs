bc040775c1988c1bd641389414be8187af11c6e7
Inwerwm <saclainvervm@gmail.com>
2021-06-23 13:15:38 +0900

    描画に必要な処理を実装

--- PEPIwUVEditor/EditController/MoveController.cs
+++ PEPIwUVEditor/EditController/MoveController.cs
@@ -1,4 +1,5 @@
 ﻿using IwUVEditor.DirectX;
+using IwUVEditor.DirectX.DrawElement;
 using IwUVEditor.StateContainer;
 using SlimDX;
 using System;
@@ -9,27 +10,71 @@ using System.Threading.Tasks;
 
 namespace IwUVEditor.EditController
 {
-    class MoveController : EditController
+    class MoveController : EditController, IDisposable
     {
-        public override Vector3 Center { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        private bool disposedValue;
+        private Vector3 center;
 
-        public MoveController(UVViewDrawProcess process) : base(process)
+        private MovingController Controller { get; }
+        public override Vector3 Center
         {
+            get => center;
+            set
+            {
+                center = value;
+                Controller.Center = value;
+            }
+        }
+
+        public MoveController(UVViewDrawProcess process, SlimDX.Direct3D11.Device device) : base(process)
+        {
+            Controller = new MovingController(device, Process.Effect, Process.Rasterize.Solid)
+            {
+                Center = Center,
+                ScreenSize = Process.ScreenSize
+            };
+
+            Process.ScreenSizeChanged += Process_ScreenSizeChanged;
+        }
+
+        private void Process_ScreenSizeChanged(Vector2 screenSize)
+        {
+            Controller.ScreenSize = screenSize;
         }
 
         public override void PrepareDrawing()
         {
-            throw new NotImplementedException();
+            Controller.Prepare();
         }
 
         protected override void ApplyModeChange(SelectionMode mode)
         {
-            throw new NotImplementedException();
+            ;
         }
 
         protected override SelectionMode CalcMode(InputStates input)
         {
-            throw new NotImplementedException();
+            return SelectionMode.None;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    Controller.Dispose();
+                    Process.ScreenSizeChanged -= Process_ScreenSizeChanged;
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }

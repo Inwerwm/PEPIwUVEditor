6a0cb456609af06d2c8fb6dc33e49315e36b14c9
Inwerwm <saclainvervm@gmail.com>
2021-06-24 16:59:33 +0900

    反転コマンドを実装

--- PEPIwUVEditor/Command/CommandReverse.cs
+++ PEPIwUVEditor/Command/CommandReverse.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PEPlugin.Pmx;
+using SlimDX;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,16 +10,37 @@ namespace IwUVEditor.Command
 {
     class CommandReverse : IEditorCommand
     {
-        public bool IsDestructive => throw new NotImplementedException();
+        public bool IsDestructive => true;
+
+        IEnumerable<IPXVertex> TargetVertices { get; }
+
+        Axis ReverseAxis { get; }
+
+        public CommandReverse(IEnumerable<IPXVertex> targetVertices, Axis axis)
+        {
+            TargetVertices = targetVertices;
+            ReverseAxis = axis;
+        }
 
         public void Do()
         {
-            throw new NotImplementedException();
+            (var min, var max) = TargetVertices.Select(vtx => vtx.UV.ToVector2()).MinMax();
+            var center = ((min + max) / 2).ToVector3();
+
+            Matrix reverser = Matrix.Invert(Matrix.Translation(center)) * Matrix.Scaling(ReverseAxis != Axis.Y ? -1 : 1, ReverseAxis != Axis.X ? -1 : 1, 1) * Matrix.Translation(center);
+            TargetVertices.AsParallel().ForAll(vtx => vtx.UV = Vector2.TransformCoordinate(vtx.UV, reverser));
         }
 
         public void Undo()
         {
-            throw new NotImplementedException();
+            Do();
+        }
+
+        public enum Axis
+        {
+            X,
+            Y,
+            Both
         }
     }
 }

0a7696defd63759cad83c3ad54fc467cd163ec9b
Inwerwm <saclainvervm@gmail.com>
2021-06-24 16:21:43 +0900

    反転コマンドクラスを生成

---
+++ PEPIwUVEditor/Command/CommandReverse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IwUVEditor.Command
+{
+    class CommandReverse : IEditorCommand
+    {
+        public bool IsDestructive => throw new NotImplementedException();
+
+        public void Do()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Undo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

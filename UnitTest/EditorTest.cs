2b9c5b751c30dc97e24cd9912171fe7907ef172b
Inwerwm <saclainvervm@gmail.com>
2021-06-26 18:42:44 +0900

    コピペテストを追加

--- UnitTest/EditorTest.cs
+++ UnitTest/EditorTest.cs
@@ -12,16 +12,50 @@ namespace UnitTest
     [TestClass]
     public class EditorTest
     {
-        Editor Editor { get; set; }
+        static Editor Editor { get; set; }
+        static V2[] UV { get; set; }
 
         [ClassInitialize]
-        public void Initialize()
+        public static void Initialize(TestContext testContext)
         {
-            var args = PEMockFactory.CreateRunArgs(new[] { new V2(0, 1), new V2(-1, -1), new V2(1, -1) }, new[] { (0, 1, 2) });
+            var args = PEMockFactory.CreateRunArgs(new[] 
+            {
+                new V2(0, 1), new V2(-1, 0), new V2(1, -1) 
+            }, new[] 
+            {
+                (0, 1, 2) 
+            });
             Editor = new Editor(args, new IwUVEditor.StateContainer.EditorStates(), null);
             Editor.LoadModel();
+
+            foreach (var key in Editor.Current.Material.IsSelected.Keys.ToArray())
+            {
+                Editor.Current.Material.IsSelected[key] = true;
+            }
+            UV = Editor.Current.Material.Vertices.Select(v => v.UV).ToArray();
         }
 
+        public void IsInitSuccess()
+        {
+            Assert.AreEqual(0, UV[0].X);
+            Assert.AreEqual(1, UV[0].Y);
+            Assert.AreEqual(-1, UV[1].X);
+            Assert.AreEqual(0, UV[1].Y);
+            Assert.AreEqual(1, UV[2].X);
+            Assert.AreEqual(-1, UV[2].Y);
+        }
 
+        [TestMethod]
+        public void TestCopyAndPaste()
+        {
+            Editor.CopyPosition();
+            Editor.PastePosition();
+
+            Assert.AreEqual(0, UV[0].X);
+            Assert.AreEqual(0, UV[0].Y);
+
+            Editor.Undo();
+            IsInitSuccess();
+        }
     }
 }

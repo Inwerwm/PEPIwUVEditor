20394c4229be789d5375dfcf1032c9bdabb86505
Inwerwm <saclainvervm@gmail.com>
2021-06-26 12:59:47 +0900

    頂点編集パラメータデータのインターフェイスを追加

---
+++ PEPIwUVEditor/Tool/IEditParameter.cs
@@ -0,0 +1,21 @@
+﻿using SlimDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IwUVEditor.Tool
+{
+    interface IEditParameter
+    {
+        Vector3 MoveCenter { get; set; }
+        Vector3 MoveOffset { get; set; }
+
+        Vector3 RotationCenter { get; set; }
+        float RotationAngle { get; set; }
+
+        Vector3 ScaleCenter { get; set; }
+        Vector3 ScaleRatio { get; set; }
+    }
+}

423fa93e73b34f8ebe0367194ab7b4ccac2cc021
Inwerwm <saclainvervm@gmail.com>
2021-06-27 22:16:37 +0900

    UV出力サイズ設定フォームを追加

---
+++ PEPIwUVEditor/Subform/FormExportUVMeshConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IwUVEditor.Subform
+{
+    public partial class FormExportUVMeshConfig : Form
+    {
+        public FormExportUVMeshConfig()
+        {
+            InitializeComponent();
+        }
+    }
+}

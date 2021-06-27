49cf5c3cfeab96e69fceb1e551f9b7d7c4f81adc
Inwerwm <saclainvervm@gmail.com>
2021-06-28 00:16:49 +0900

    Drawerフォルダーと名前空間の作成とUVExporterの移動

--- PEPIwUVEditor/Drawer/UVExporter.cs
+++ PEPIwUVEditor/Drawer/UVExporter.cs
@@ -1,11 +1,12 @@
 ﻿using PEPlugin.Pmx;
+using SlimDX.Direct2D;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IwUVEditor
+namespace IwUVEditor.Drawer
 {
     class UVExporter
     {

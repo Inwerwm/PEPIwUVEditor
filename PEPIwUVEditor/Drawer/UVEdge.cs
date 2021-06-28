10fa7f1c01df393af79313a9f74942d24b31eb05
Inwerwm <saclainvervm@gmail.com>
2021-06-28 09:36:17 +0900

    頂点ではなくUV位置を保持するようにして掛け算演算子を追加

--- PEPIwUVEditor/Drawer/UVEdge.cs
+++ PEPIwUVEditor/Drawer/UVEdge.cs
@@ -1,15 +1,21 @@
 ﻿using PEPlugin.Pmx;
 using System.Collections.Generic;
+using System.Drawing;
 
 namespace IwUVEditor.Drawer
 {
     class UVEdge
     {
-        public IPXVertex[] VertexPair { get; }
+        public PointF[] UV { get; }
 
         public UVEdge(IPXVertex vertex1, IPXVertex vertex2)
         {
-            VertexPair = new IPXVertex[2] { vertex1, vertex2 };
+            UV = new[] { new PointF(vertex1.UV.X, vertex1.UV.Y), new PointF(vertex2.UV.X, vertex2.UV.Y) };
+        }
+
+        private UVEdge(PointF point1, PointF point2)
+        {
+            UV = new[] { point1, point2 };
         }
 
         public static IEnumerable<UVEdge> FromFace(IPXFace face) => new[]
@@ -18,5 +24,11 @@ namespace IwUVEditor.Drawer
             new UVEdge(face.Vertex2, face.Vertex3),
             new UVEdge(face.Vertex3, face.Vertex1)
         };
+
+        public static UVEdge operator *(UVEdge edge, float ratio) =>
+            new UVEdge(
+                new PointF(edge.UV[0].X * ratio, edge.UV[0].Y * ratio),
+                new PointF(edge.UV[1].X * ratio, edge.UV[1].Y * ratio)
+            );
     }
 }

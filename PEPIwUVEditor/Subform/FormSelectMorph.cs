80846a867566972423eb2b0010fffd3de59ab9ad
Inwerwm <saclainvervm@gmail.com>
2021-07-09 21:20:21 +0900

    モーフ選択フォームを実装

---
+++ PEPIwUVEditor/Subform/FormSelectMorph.cs
@@ -0,0 +1,37 @@
+﻿using PEPlugin.Pmx;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IwUVEditor.Subform
+{
+    public partial class FormSelectMorph : Form
+    {
+        public IPXMorph SelectedMorph => listBoxMorphs.SelectedItem as IPXMorph;
+
+        public FormSelectMorph(IEnumerable<IPXMorph> morphs)
+        {
+            InitializeComponent();
+
+            listBoxMorphs.Items.AddRange(morphs.ToArray());
+            DialogResult = DialogResult.Cancel;
+        }
+
+        private void listBoxMorphs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            buttonOk.Enabled = SelectedMorph != null;
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
